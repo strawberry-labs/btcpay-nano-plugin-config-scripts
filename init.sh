1a1f83373e4909dc1cd92ec6242ff629d982cfd4
shriram-sg <shriramsekar11@gmail.com>
2025-11-04 11:26:53 +0400

    fix: update ledger link in init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget -O ledger.7z https://mynano.ninja/api/ledger/download -q --show-progress
+wget -O ledger.7z https://s3.us-east-2.amazonaws.com/repo.nano.org/snapshots/latest -q --show-progress
+# wget -O ledger.7z https://mynano.ninja/api/ledger/download -q --show-progress
 # wget -O test https://github.com/strawberry-labs/BTCPay-Nano-Plugin/blob/master/LICENSE -q --show-progress
 
 apt install p7zip -y
