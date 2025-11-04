e6dfac57f179deedb37144b0b5991204c7ff1cfe
shriram-sg <shriramsekar11@gmail.com>
2025-11-04 11:16:47 +0400

    feat: update the init script to have actual file download

--- init.sh
+++ init.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# wget -O ledger.7z https://mynano.ninja/api/ledger/download -q --show-progress
-wget -O test https://github.com/strawberry-labs/BTCPay-Nano-Plugin/blob/master/LICENSE -q --show-progress
- 
-# p7zip -d ledger.7z
- 
-# mv data.ldb Nano/data.ldb
+wget -O ledger.7z https://mynano.ninja/api/ledger/download -q --show-progress
+# wget -O test https://github.com/strawberry-labs/BTCPay-Nano-Plugin/blob/master/LICENSE -q --show-progress
 
-mv test ~/Nano/test
+apt install p7zip -y
  
-# docker restart nano-node
+p7zip -d ledger.7z
+ 
+mv data.ldb ~/Nano/data.ldb
+
+# mv test ~/Nano/test
+ 
+docker restart btcpayserver_nano_node
