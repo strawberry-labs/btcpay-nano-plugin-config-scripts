cdfdd4dae099ced16b966c8f4b56b8eeb9475ff9
shriram-sg <shriramsekar11@gmail.com>
2025-11-20 16:11:51 +0530

    fix: add echo statements to the file and fix some commands

--- init.sh
+++ init.sh
@@ -3,39 +3,48 @@ echo "Please enter the absolute path for the location for processing the set-up 
 read location
 
 #get url for latest ledger file
+echo -e "\nFetching ledger link file \n"
 wget -O ledger-link https://s3.us-east-2.amazonaws.com/repo.nano.org/snapshots/latest -q --show-progress
 
 LEDGER_LINK="$(cat ledger-link)"
 
 #download ledger file
+echo -e "\nDownloading nano ledger file \n"
 wget -O ledger.7z $LEDGER_LINK -q --show-progress
 
-apt install p7zip -y
+#echo -e "\nInstalling p7zip \n"
+#apt install p7zip -y
 
-cd $location
+#cd $location
 
-p7zip -d ~/btcpay-nano-plugin-config-scripts/ledger.7z 
- 
+#echo -e "\nUnzipping the ledger file \n"
+#p7zip -d ~BTCPayServer/btcpay-nano-plugin-config-scripts/ledger.7z 
+
+echo -e "\nMoving nano ledger file to the Nano folder \n"
 mv ./data.ldb ~/Nano/data.ldb
 
 cd ~
 
 # Enable wallet rpc
+echo -e "\nEditing the nano config-rpc.toml file \n"
 sed -i 's/enable_control = false/enable_control = true/' ~/Nano/config-rpc.toml
 
 # Disable node work gen
-sed -i 's/work_threads = 1/work_threads = 0/' ~/Nano/config-node.toml
+echo -e "\nEditing the nano config-node.toml file \n"
+echo -e "work_threads = 0" >> ~/Nano/config-node.toml
 
 # Configure pippin wallet
+echo -e "\nEditing the pippin config file \n"
 sed -i \
   -e 's/host: 127.0.0.1/host: 0.0.0.0/' \
   -e 's/node_rpc_url: *http:\/\/\[::1\]:7076/node_rpc_url: "http:\/\/btcpayserver_nano_node:7076"/' \
   -e 's/node_ws_url: *""/node_ws_url: "ws:\/\/btcpayserver_nano_node:7078"/' \
-  -e 's/work_peers: *\[\]/work_peers: ["https://uk1.public.xnopay.com/proxy"]/' \
+  -e 's/work_peers: *\[\]/work_peers: ["https:\/\/uk1.public.xnopay.com\/proxy"]/' \
   -e 's/receive_minimum: "1000000000000000000000000"/receive_minimum: "1"/' \
-  -e 's/work_timeout: 30"/work_timeout: 120/' \
+  -e 's/work_timeout: "30"/work_timeout: "120"/' \
   ~/PippinData/config.yaml
  
+echo -e "\nRestarting nano node and pippin \n"
 docker restart btcpayserver_nano_node
 
 docker restart btcpayserver_nano_pippin_wallet
